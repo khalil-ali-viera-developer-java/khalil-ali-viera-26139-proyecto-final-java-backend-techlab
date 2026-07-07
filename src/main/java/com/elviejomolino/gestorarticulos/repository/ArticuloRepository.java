29c3ebf0f133cfd5bb0197d71e63ff6791671ef8
khalil <khalil.ali.viera@gmail.com>
2026-07-07 10:36:08 -0300

    La interface ArticuloRepository la modifique para poder extender de JpaRepository y asi utilizar sus métodos. Agregue las anotaciones correspondientes

--- src/main/java/com/elviejomolino/gestorarticulos/repository/ArticuloRepository.java
+++ src/main/java/com/elviejomolino/gestorarticulos/repository/ArticuloRepository.java
@@ -1,9 +1,19 @@
 package com.elviejomolino.gestorarticulos.repository;
 
-public interface ArticuloRepository { // INICIO CLASE ARTICULOREPOSITORY;
+// IMPORTO JPAREPOSITORY;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// IMPORTO REPOSITORY;
+import org.springframework.stereotype.Repository;
+
+// IMPORTO ARTICULO;
+import com.elviejomolino.gestorarticulos.model.Articulo;
+
+@Repository
+public interface ArticuloRepository extends JpaRepository<Articulo, Long> { // INICIO INTERFACE ARTICULOREPOSITORY;
 
     // ESTADO: ATRIBUTOS;
 
     // COMPORTAMIENTO: CONSTRUCTORES - MÉTODOS;
 
-} // FINAL CLASE ARTICULOREPOSITORY;
+} // FINAL INTERFACE ARTICULOREPOSITORY;
